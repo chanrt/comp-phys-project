ef0198a753c4a2da49d07d7b377becd570959b7f
Chess Wizard <chandan.relekar@gmail.com>
2022-04-26 06:03:52 +0530

    Change rho function and wrote cluster code

--- src/automaton.py
+++ src/automaton.py
@@ -1,9 +1,11 @@
 # Simulates the vegetation automaton
 # Shows animation
+# Saves data in automaton_data
 
 from itertools import product
+from math import sqrt
 from numba import njit
-from numpy import copy, sum, zeros
+from numpy import array, copy, sum, zeros
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
 import os
@@ -13,42 +15,50 @@ from random import random
 
 @njit(fastmath=True, nogil=False)
 def mc_step(lattice):
+    """ Simulates a single Monte Carlo step of the automaton """
     n = len(lattice)
     f_current = sum(lattice) / (n * n)
-
     num_updates = int(mc_fraction * n * n)
 
     for _ in range(num_updates):
         i = int(random() * n)
         j = int(random() * n)
-        rho = get_rho(lattice, i, j)
+        rho = get_density(lattice, i, j)
 
         if lattice[i, j] == 0:
             prob_growth = rho + (f_carrying - f_current) / (1 - f_current)
             if random() < prob_growth:
                 lattice[i, j] = 1
         else:
-            prob_decay = rho + (f_current - f_carrying) / f_current
+            prob_decay = (1 - rho) + (f_current - f_carrying) / f_current
             if random() < prob_decay:
                 lattice[i, j] = 0
-    return lattice
 
 
 @njit(fastmath=True, nogil=False)
-def get_rho(lattice, i, j):
+def get_density(lattice, i, j):
+    """ Calculates the vegetation density in the neighbourhood of a given cell (i, j) """
     n = len(lattice)
-    s = 0
-    rho = 0
+    normalization = 0
+    density = 0
 
     for a in range(i - r_influence, i + r_influence + 1):
         for b in range(j - r_influence, j + r_influence + 1):
             if 0 < a < n and 0 < b < n and (i - a) ** 2 + (j - b) ** 2 < r_influence ** 2:
-                rho += lattice[a, b]
-                s += 1
-    return rho / s
+                weightage_term = 1 - get_distance(i, j, a, b) / k
+                density += weightage_term * lattice[a, b]
+                normalization += weightage_term
+    return density / normalization
+
+
+@njit
+def get_distance(i, j, a, b):
+    """ Calculates the distance between two cells at locations (i, j) and (a, b) """
+    return sqrt((i - a)**2 + (j - b)**2)
 
 
 def make_initial_lattice(n):
+    """ Generates a random initial lattice with occupancy around 50% """
     lattice = zeros((n, n), dtype=int)
     for i, j in product(range(n), range(n)):
         lattice[i, j] = 1 if random() > 0.5 else 0
@@ -56,56 +66,76 @@ def make_initial_lattice(n):
 
 
 def get_forest_cover(rainfall):
-    # this fit was done in linear_regression.py
+    """ Calculates the forest cover for a given value of rainfall, based on a linear fit """
     slope = 0.0008588
     intercept = -0.1702
     return slope * rainfall + intercept
 
 
-def save_data(lattice_record):
-    num_files = len([name for name in os.listdir(os.path.join(os.path.dirname(__file__), 'automaton_data'))])
-    file_name = 'simulation_{}.pkl'.format(num_files)
-    path_name = os.path.join(os.path.dirname(__file__), 'automaton_data', file_name)
-    dump(lattice_record, open(path_name, 'wb'))
+def save_automaton_data(lattice_record):
+    """ Saves the entire simulation data in a pickle file under automaton_data """
+    current_path = os.path.dirname(__file__)
+    files_list = os.listdir(os.path.join(current_path, "automaton_data"))
+
+    num_automaton_simulations = 0
+    for file_name in files_list:
+        if file_name.startswith("simulation_") and file_name.endswith(".pkl"):
+            num_automaton_simulations += 1
+
+    file_name = 'simulation_{}.pkl'.format(num_automaton_simulations)
+    path_name = os.path.join(current_path, 'automaton_data', file_name)
+    dump(array(lattice_record, dtype=bool), open(path_name, 'wb'))
 
 
 def animate(i):
+    """" Animates a given frame (i) of the simulation """
     im.set_array(lattice_record[i])
     return [im]
 
 
 if __name__ == '__main__':
+    #############
+    # CONSTANTS #
+    #############
     n = 500  # dimensions of lattice
     mc_steps = 200  # in years
     mc_fraction = 0.2  # 20% of the cells are updated in every step
     rainfall = 500  # in mm
     f_carrying = get_forest_cover(rainfall)
-    r_influence = 4  # radius of influence
+    r_influence = 5  # radius of influence
+    k = 24  # measure of decrease in weightage of neighbours
 
+    ###########
+    # LATTICE #
+    ###########
     lattice = make_initial_lattice(n)
     lattice_record = []
 
+    ##############
+    # SIMULATION #
+    ##############
     print("Compiling functions (will take a few seconds) ...")
     for step in range(mc_steps):
         print(f"Year {step}")
         mc_step(lattice)
         lattice_record.append(copy(lattice))
 
-    save_data(lattice_record)
+    save_automaton_data(lattice_record)
+
+    #############
+    # ANIMATION #
+    #############
     fig = plt.figure()
     im = plt.imshow(lattice_record[0])
-
     num_frames = len(lattice_record)
-
     animate = FuncAnimation(fig,
                             animate,
                             frames=num_frames,
-                            interval=10,
+                            interval=50,
                             repeat=False)
     plt.show()
 
     forest_cover = [sum(lattice) / (n * n) for lattice in lattice_record]
-    
     plt.title("Change of forest cover with time")
     plt.xlabel("Time (years)")
     plt.ylabel("Forest cover")
--- src/cluster.py
+++ src/cluster.py
@@ -1,2 +1,101 @@
 # Goes through every saved data file from automaton_data
 # Generates a time series data of number of clusters of each size, for every year
+
+from matplotlib import pyplot as plt
+from numpy import array, zeros
+from pickle import dump, load
+import os
+
+
+def cluster(lattice):
+    """ Calculates number of clusters of each size, in the given lattce """
+    n = len(lattice)
+    cluster_sizes = zeros((n * n + 1), dtype=(int))
+    visited = zeros((n, n), dtype=(int, int))
+
+    for i in range(n):
+        for j in range(n):
+            if not visited[i][j]:
+                visited[i][j] = 1
+
+                if lattice[i][j] == 0:
+                    cluster_sizes[0] += 1
+                    continue
+
+                current_cluster_size = 1
+                stack = [(i, j)]
+
+                while len(stack) > 0:
+                    i, j = stack.pop()
+                    von_neumann_neighbours = [(i - 1, j), (i + 1, j), (i, j - 1),
+                                            (i, j + 1)]
+
+                    for i, j in von_neumann_neighbours:
+                        if 0 <= i < n and 0 <= j < n:
+                            if lattice[i][j] == 1 and not visited[i][j]:
+                                visited[i][j] = True
+                                stack.append((i, j))
+                                current_cluster_size += 1
+                cluster_sizes[current_cluster_size] += 1
+
+    return cluster_sizes
+
+
+def check_automaton_data():
+    """ Checks how many simulation files are present in automaton_data """
+    current_path = os.path.dirname(__file__)
+    files_list = os.listdir(os.path.join(current_path, "automaton_data"))
+
+    num_automaton_simulations = 0
+    for file_name in files_list:
+        if file_name.startswith("simulation_") and file_name.endswith(".pkl"):
+            num_automaton_simulations += 1
+
+    return num_automaton_simulations
+
+
+def load_automaton_data(num_file):
+    """ Loads the data from the simulation file with the given number """
+    num_simulations = check_automaton_data()
+    if num_file >= num_simulations:
+        raise ValueError("Invalid simulation file number")
+
+    current_path = os.path.dirname(__file__)
+    file_name = os.path.join(current_path, "automaton_data",
+                             "simulation_{}.pkl".format(num_file))
+
+    with open(file_name, "rb") as file:
+        return load(file)
+
+
+def save_cluster_data(cluster_sizes_record, simulation_index):
+    """ Saves the data of number of clusters of each size, for every year """
+    current_path = os.path.dirname(__file__)
+    file_name = os.path.join(current_path, "cluster_data",
+                             "cluster_data_{}.pkl".format(simulation_index))
+
+    dump(array(cluster_sizes_record, dtype=int), open(file_name, 'wb'))
+
+
+if __name__ == '__main__':
+    # index of the simulation whose clustering time series data must be generated
+    simulation_index = int(input("Enter simulation index: "))
+
+    lattice_record = load_automaton_data(simulation_index)
+    cluster_sizes_record = []
+
+    for i, lattice in enumerate(lattice_record):
+        print("Lattice {}".format(i))
+        cluster_sizes_record.append(cluster(lattice))
+
+    save_cluster_data(cluster_sizes_record, simulation_index)
+
+    # lattice = [
+    #     [0, 0, 0, 0, 1],
+    #     [0, 1, 1, 0, 0],
+    #     [0, 1, 0, 1, 0],
+    #     [0, 0, 1, 1, 1],
+    #     [0, 0, 0, 0, 0]
+    # ]
+
+    # print(cluster(lattice))
